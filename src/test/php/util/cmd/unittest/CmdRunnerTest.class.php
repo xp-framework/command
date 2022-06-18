47588ff85c94502f4d1f7af263594e3afaa59dae
Timm Friebe <thekid@thekid.de>
2022-06-18 13:45:49 +0200

    Start REPL if `xp cmd` is invoked without a class

--- src/test/php/util/cmd/unittest/CmdRunnerTest.class.php
+++ src/test/php/util/cmd/unittest/CmdRunnerTest.class.php
@@ -8,9 +8,9 @@ class CmdRunnerTest extends AbstractRunnerTest {
   /** @return xp.command.AbstractRunner */
   protected function runner() { return new CmdRunner(); }
 
-  #[Test, Values([[[]], [['-?']]])]
-  public function selfUsage($args) {
-    $return= $this->runWith($args);
+  #[Test]
+  public function selfUsage() {
+    $return= $this->runWith(['-?']);
     $this->assertEquals(1, $return);
     $this->assertOnStream($this->err, 'xp cmd [class]');
     $this->assertEquals('', $this->out->bytes());
@@ -34,5 +34,12 @@ class CmdRunnerTest extends AbstractRunnerTest {
     $this->assertOnStream($this->err, '$ xp cmd '.nameof($command));
     $this->assertEquals('', $this->out->bytes());
     $this->assertFalse($command->wasRun());
+  }
+
+  #[Test]
+  public function startRepl() {
+    $return= $this->runWith([], 'exit');
+    $this->assertEquals(0, $return);
+    $this->assertOnStream($this->out, 'XP Command REPL');
   }
 }
